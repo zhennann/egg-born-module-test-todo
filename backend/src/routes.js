396766d4a6af6fb8b0798721fd7cb0002c92e70c
zhennann <zhen.nann@icloud.com>
2019-04-19 16:11:27 +0800

    feat: atom.write

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,13 +8,18 @@ module.exports = app => {
     { method: 'post', path: 'version/init', controller: version, middlewares: 'inner' },
     { method: 'post', path: 'version/test', controller: version, middlewares: 'test' },
     // todo
-    { method: 'post', path: 'todo/create', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/read', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/select', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/write', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/delete', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/action', controller: todo, middlewares: 'inner' },
-    { method: 'post', path: 'todo/enable', controller: todo, middlewares: 'inner' },
+    { method: 'post', path: 'todo/create', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'todo/read', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'todo/select', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'todo/write', controller: todo, middlewares: 'inner,validate',
+      meta: {
+        auth: { enable: false },
+        validate: { validator: 'todo', data: 'item' },
+      },
+    },
+    { method: 'post', path: 'todo/delete', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'todo/action', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'todo/enable', controller: todo, middlewares: 'inner', meta: { auth: { enable: false } } },
   ];
   return routes;
 };
